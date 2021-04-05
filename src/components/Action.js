78b2c3aa819f91402986ef205f01675408609ce7
Stackbit Code Editor <projects@stackbit.com>
2021-04-05 17:45:21 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -11,15 +11,7 @@ export default class Action extends React.Component {
         let action_icon = _.get(action, 'icon', null) || 'arrow-left';
         let action_icon_pos = _.get(action, 'icon_position', null) || 'left';
         return (
-            <Link href={withPrefix(_.get(action, 'url', null))}
-              {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
-              {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
-              className={classNames({'button': (action_style === 'primary') || (action_style === 'secondary'), 'secondary': action_style === 'secondary', 'has-icon': _.get(action, 'has_icon', null)})}>
-              {_.get(action, 'has_icon', null) && (
-                <Icon {...this.props} icon={action_icon} />
-              )}
-              <span className={classNames({'order-first': action_icon_pos === 'right'})}>{_.get(action, 'label', null)}</span>
-            </Link>
+            <script id="setmore_script" type="text/javascript" src="https://my.setmore.com/webapp/js/src/others/setmore_iframe.js"></script><a id="Setmore_button_iframe" style="float:none" href="https://my.setmore.com/bookingpage/2433fc45-ddd4-4e7d-bce3-6346cbad6db8"><img border="none" src="https://storage.googleapis.com/setmore-assets/2.0/Images/Integration/book-now-blue.svg" alt="Book an appointment with CreativePixel using Setmore" /></a>
         );
     }
 }
