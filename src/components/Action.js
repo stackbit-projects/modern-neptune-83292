93d047406c5aa0dc44a1f993c839c56a698e2f65
Stackbit Code Editor <projects@stackbit.com>
2021-04-05 17:46:02 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -11,7 +11,6 @@ export default class Action extends React.Component {
         let action_icon = _.get(action, 'icon', null) || 'arrow-left';
         let action_icon_pos = _.get(action, 'icon_position', null) || 'left';
         return (
-            <script id="setmore_script" type="text/javascript" src="https://my.setmore.com/webapp/js/src/others/setmore_iframe.js"></script><a id="Setmore_button_iframe" style="float:none" href="https://my.setmore.com/bookingpage/2433fc45-ddd4-4e7d-bce3-6346cbad6db8"><img border="none" src="https://storage.googleapis.com/setmore-assets/2.0/Images/Integration/book-now-blue.svg" alt="Book an appointment with CreativePixel using Setmore" /></a>
         );
     }
 }
